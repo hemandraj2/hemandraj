00db0192340d6fd1be081c45214e30a1322c6969
Hemand Raj <41236675+HemandRaj@users.noreply.github.com>
2021-07-21 22:40:47 +0530

    normal

--- src/app/portfolio/Components/treelist/treelist.component.ts
+++ src/app/portfolio/Components/treelist/treelist.component.ts
@@ -31,19 +31,19 @@ export class TreelistComponent implements OnInit {
             "name": "RESUME MODEL",
             "sub_cat": [
               {
-                "id":111,
+                "id": 111,
                 "name": "RESUME MODEL inner1",
                 "sub_cat": [
                   {
-                    "id":1111,
+                    "id": 1111,
                     "name": "RESUME MODEL inner2",
                     "sub_cat": [
                       {
-                        "id":11111,
+                        "id": 11111,
                         "name": "RESUME MODEL inner3",
                         "sub_cat": [
                           {
-                            "id":111111,
+                            "id": 111111,
                             "name": "RESUME MODEL inner4",
                           }
                         ]
@@ -63,15 +63,15 @@ export class TreelistComponent implements OnInit {
             "name": "CONTACT MODEL",
             "sub_cat": [
               {
-                "id":131,
+                "id": 131,
                 "name": "RESUME MODEL inner",
                 "sub_cat": [
                   {
-                    "id":1311,
+                    "id": 1311,
                     "name": "RESUME MODEL inner",
                     "sub_cat": [
                       {
-                        "id":13111,
+                        "id": 13111,
                         "name": "RESUME MODEL inner",
                       }
                     ]
@@ -120,38 +120,18 @@ export class TreelistComponent implements OnInit {
   }
 
   specificTraverse(id) {
-    let flag = true;
     let temp = id.toString();
     let tempArray = [];
+    let listArray = [];
     for (var i = 0, len = temp.length; i < len; i += 1) {
       tempArray.push(+temp.charAt(i));
     }
-    console.log(tempArray)
     tempArray = tempArray.reverse();
     console.log(tempArray)
-
-  //   for (let i = 0; i < traverse_data.length; i++) {
-  //     if (traverse_data[i].sub_cat) {
-  //       if (traverse_data[i].id == id) {
-  //         if (this.currentList[i].sub_cat) {
-  //           this.currentList = this.currentList[i].sub_cat;
-  //         }
-  //         else {
-  //           this.router.navigate(['item-data'])
-  //         }
-  //         flag = false;
-  //       }
-  //       else {
-  //         traverse_data = traverse_data[i].sub_cat;
-  //       }
-  //     }
-  //     else {
-  //       traverse_data = traverse_data[i].sub_cat = null;
-  //       traverse_data = this.listStructure;
-  //       break;
-  //     }
-  //   }
-  // }
+    for (let i = 0; i < tempArray.length; i++) {
+      listArray = this.listStructure[tempArray[i]].sub_cat;
+    }
+    this.currentList = listArray;
   }
 
 
