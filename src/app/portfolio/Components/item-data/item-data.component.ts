abd773387fba7cfd081f336976f0d914da3f666b
Hemand Raj <41236675+HemandRaj@users.noreply.github.com>
2021-07-27 09:01:49 +0530

    normal

--- src/app/portfolio/Components/item-data/item-data.component.ts
+++ src/app/portfolio/Components/item-data/item-data.component.ts
@@ -9,6 +9,13 @@ import { CommonService } from '../../../Services/common.service';
 })
 export class ItemDataComponent implements OnInit {
 
+  //Multiselect_Dropdown
+  multiDropdown: string;
+  multiBoxActive: boolean = false;
+  multidropdownData: string[];
+  multidropdownDataTemp: string[];
+  multiselectedValue: string;
+
   //Custom_Dropdown
   customDropdown: string;
   suggestionBoxActive: boolean = false;
@@ -23,6 +30,10 @@ export class ItemDataComponent implements OnInit {
   constructor(private service: CommonService) { }
 
   ngOnInit(): void {
+
+    //Custom_Dropdown
+    this.multidropdownData = this.service.getDropDownData();
+    this.multidropdownDataTemp = this.multidropdownData;
 
     //Custom_Dropdown
     this.dropdownData = this.service.getDropDownData();
@@ -63,5 +74,38 @@ export class ItemDataComponent implements OnInit {
     this.customDropdown = data;
     this.selectedValue = data
     this.suggestionBoxActive = false;
+  }
+
+  //Multiselect_Dropdown
+  multiDropdownClicked() {
+    console.log("Dropdown Clicked");
+    this.multiBoxActive = true;
+  }
+  multiDropdownChanged() {
+    console.log("Dropdown Changed");
+    this.multiBoxActive = false;
+  }
+  multiDropdownTyped() {
+    console.log("Typed in custom Dropdown");
+    this.multidropdownDataTemp = this.multidropdownData.filter(a => ((a.toUpperCase()).includes(this.multiDropdown.toUpperCase())));
+    this.multiBoxActive = true;
+  }
+  multiDropdownFocused() {
+    console.log("Custom Dropdown Focused");
+    this.multiBoxActive = true;
+  }
+  multiDropdownInputBlur() {
+    console.log("Dropdown Input Blured");
+    setTimeout(() => {
+      if (this.multiselectedValue == this.multiDropdown) {
+        this.multiBoxActive = false;
+      }
+    }, 500);
+  }
+  multiItemClicked(data) {
+    console.log("Item Clicked");
+    this.multiDropdown = data;
+    this.multiselectedValue = data
+    this.multiBoxActive = false;
   }
 }
